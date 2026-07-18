4d217a04bc82ec204740c2238bffb599761d4910
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-07-18 01:41:35 +0000

    Factor sign side proof term

--- RealRooted/Tactic/Sign.lean
+++ RealRooted/Tactic/Sign.lean
@@ -403,12 +403,16 @@ namespace Tactic
 
 syntax (name := rr_sign_side) "rr_sign_side" : tactic
 
+syntax (name := rr_sign_side_term) "rr_sign_side_term" : term
+
 macro_rules
   | `(tactic| rr_sign_side) =>
       `(tactic|
         first
           | rr_side_nonneg
           | rr_side)
+  | `(rr_sign_side_term) =>
+      `(by rr_sign_side)
 
 syntax (name := rr_sign) "rr_sign" : tactic
 
@@ -420,171 +424,171 @@ macro_rules
           | assumption
           | exact
               RealRooted.eval_X_mul_one_add_four_mul_X_nonpos_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_one_add_four_mul_X_nonpos_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact RealRooted.eval_X_nonpos_of_nonpos
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_nonpos_of_nonneg_of_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_one_sub_X_nonpos_of_nonneg_of_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact RealRooted.eval_X_mul_one_sub_X_nonpos_of_nonpos
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact
               RealRooted.eval_X_mul_C_sub_X_nonpos_of_nonneg_of_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_C_sub_X_nonpos_of_nonneg_of_nonneg_of_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_mul_C_sub_C_mul_X_nonpos_of_nonneg_of_nonneg_of_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_C_sub_C_mul_X_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact RealRooted.eval_one_add_X_nonpos_of_le_neg_one
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_one_add_X_nonpos_of_nonneg_of_le_neg_one
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact RealRooted.eval_C_add_X_nonpos_of_le_neg
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_C_add_X_nonpos_of_nonneg_of_le_neg
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_neg_C_mul_one_add_X_nonpos_of_nonneg_of_ge_neg_one
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_neg_C_mul_one_add_two_mul_X_nonpos_of_nonneg_of_ge_neg_half
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact RealRooted.eval_X_sub_one_nonpos_of_le_one
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_sub_one_nonpos_of_nonneg_of_le_one
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_mul_one_add_X_nonpos_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_one_add_X_nonpos_of_nonneg_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_mul_one_add_C_mul_X_nonpos_of_nonpos_of_linear_nonneg
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_one_add_C_mul_X_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_sq_sub_one_nonpos_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_sq_sub_one_nonpos_of_nonneg_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_neg_C_mul_X_mul_one_add_X_nonpos_of_nonneg_of_le_neg_one
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_one_add_X_mul_one_add_two_mul_X_nonpos_of_mem_interval
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_one_add_X_mul_one_add_two_mul_X_nonpos_of_nonneg_of_mem_interval
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_mul_one_sub_X_sq_nonpos_of_nonpos
-                (by rr_sign_side)
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_one_sub_X_sq_nonpos_of_nonneg_of_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_mul_one_sub_X_mul_one_add_X_nonpos_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_one_sub_X_mul_one_add_X_nonpos_of_nonneg_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_sub_X_pow_three_nonpos_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_sub_X_pow_three_nonpos_of_nonneg_of_mem_Icc
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_X_mul_nonpos_of_nonpos_of_nonneg
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_C_mul_X_mul_nonpos_of_nonneg_of_nonpos_of_nonneg
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_neg_C_mul_one_sub_X_sq_nonpos_of_nonneg
-                (by rr_sign_side)
+                rr_sign_side_term
           | exact
               RealRooted.eval_neg_C_mul_X_sq_nonpos_of_nonneg
-                (by rr_sign_side)
+                rr_sign_side_term
           | exact
               RealRooted.eval_neg_C_mul_one_add_X_sq_nonpos_of_nonneg
-                (by rr_sign_side)
+                rr_sign_side_term
           | exact RealRooted.eval_C_neg_nonpos_of_nonneg
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact RealRooted.eval_neg_C_nonpos_of_nonneg
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact RealRooted.eval_neg_C_mul_sq_nonpos_of_nonneg
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact RealRooted.eval_neg_monic_quadratic_nonpos_of_discrim_nonpos
-              (by rr_sign_side)
+              rr_sign_side_term
           | exact
               RealRooted.eval_neg_C_mul_monic_quadratic_nonpos_of_nonneg_of_discrim_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
           | exact
               RealRooted.eval_neg_quadratic_nonpos_of_discrim_nonpos
-                (by rr_sign_side)
-                (by rr_sign_side)
-                (by rr_sign_side)
+                rr_sign_side_term
+                rr_sign_side_term
+                rr_sign_side_term
           | simp_all [
               Polynomial.eval_add,
               Polynomial.eval_sub,
