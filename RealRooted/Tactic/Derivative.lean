8df20187391deb746dbbf510493ad5e5f21d4380
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-07-18 18:23:25 +0000

    Add derivative tactic auto side goals

--- RealRooted/Tactic/Derivative.lean
+++ RealRooted/Tactic/Derivative.lean
@@ -12,16 +12,28 @@ open Lean.Elab.Tactic
 namespace RealRooted
 namespace Tactic
 
+syntax (name := rr_derivative_degree_side) "rr_derivative_degree_side" : tactic
+
 syntax (name := rr_derivative_interlaces_named)
   "rr_derivative_interlaces" " using "
     "splits" ":=" term ","
     "degree_two" ":=" term :
   tactic
 
+syntax (name := rr_derivative_interlaces_auto)
+  "rr_derivative_interlaces" " using "
+    "splits" ":=" term :
+  tactic
+
 syntax (name := rr_derivative_prec_named)
   "rr_derivative_prec" " using "
     "splits" ":=" term ","
     "degree_two" ":=" term :
+  tactic
+
+syntax (name := rr_derivative_prec_auto)
+  "rr_derivative_prec" " using "
+    "splits" ":=" term :
   tactic
 
 syntax (name := rr_nonneg_coeffs_derivative_named)
@@ -35,22 +47,47 @@ syntax (name := rr_pos_lc_derivative_named)
     "degree_ne_zero" ":=" term :
   tactic
 
+syntax (name := rr_pos_lc_derivative_auto)
+  "rr_pos_lc_derivative" " using "
+    "pos_lc" ":=" term :
+  tactic
+
 syntax (name := rr_derivative_ne_zero_named)
   "rr_derivative_ne_zero" " using "
     "degree_ne_zero" ":=" term :
   tactic
 
+syntax (name := rr_derivative_ne_zero_auto)
+  "rr_derivative_ne_zero" :
+  tactic
+
 macro_rules
+  | `(tactic| rr_derivative_degree_side) =>
+      `(tactic|
+        first
+          | assumption
+          | lia
+          | nlinarith
+          | rr_side <;> done)
   | `(tactic|
       rr_derivative_interlaces using
         splits := $hsplits:term,
         degree_two := $hdeg:term) =>
       `(tactic| exact RealRooted.derivative_interlaces $hsplits $hdeg)
   | `(tactic|
+      rr_derivative_interlaces using
+        splits := $hsplits:term) =>
+      `(tactic| exact RealRooted.derivative_interlaces $hsplits (by rr_derivative_degree_side))
+  | `(tactic|
       rr_derivative_prec using
         splits := $hsplits:term,
         degree_two := $hdeg:term) =>
       `(tactic| exact (RealRooted.derivative_interlaces $hsplits $hdeg).toPrec)
+  | `(tactic|
+      rr_derivative_prec using
+        splits := $hsplits:term) =>
+      `(tactic|
+        exact (RealRooted.derivative_interlaces $hsplits (by rr_derivative_degree_side)).toPrec)
   | `(tactic|
       rr_nonneg_coeffs_derivative using
         nonneg_coeffs := $hnn:term) =>
@@ -61,9 +98,17 @@ macro_rules
         degree_ne_zero := $hdeg:term) =>
       `(tactic| exact RealRooted.HasPosLeadingCoeff.derivative $hpos $hdeg)
   | `(tactic|
+      rr_pos_lc_derivative using
+        pos_lc := $hpos:term) =>
+      `(tactic|
+        exact RealRooted.HasPosLeadingCoeff.derivative $hpos (by rr_derivative_degree_side))
+  | `(tactic|
       rr_derivative_ne_zero using
         degree_ne_zero := $hdeg:term) =>
       `(tactic| exact RealRooted.derivative_ne_zero_of_natDegree_ne_zero $hdeg)
+  | `(tactic| rr_derivative_ne_zero) =>
+      `(tactic|
+        exact RealRooted.derivative_ne_zero_of_natDegree_ne_zero (by rr_derivative_degree_side))
 
 end Tactic
 end RealRooted
--- RealRooted/Tactic/Examples/Derivative.lean
+++ RealRooted/Tactic/Examples/Derivative.lean
@@ -18,10 +18,20 @@ example {p : ℝ[X]} (hsplits : p.Splits) (hdeg : 2 ≤ p.natDegree) :
     degree_two := hdeg
 
 example {p : ℝ[X]} (hsplits : p.Splits) (hdeg : 2 ≤ p.natDegree) :
+    Interlaces p.derivative p := by
+  rr_derivative_interlaces using
+    splits := hsplits
+
+example {p : ℝ[X]} (hsplits : p.Splits) (hdeg : 2 ≤ p.natDegree) :
     Prec p.derivative p := by
   rr_derivative_prec using
     splits := hsplits,
     degree_two := hdeg
+
+example {p : ℝ[X]} (hsplits : p.Splits) (hdeg : 2 ≤ p.natDegree) :
+    Prec p.derivative p := by
+  rr_derivative_prec using
+    splits := hsplits
 
 example {p : ℝ[X]} (hnn : HasNonnegCoeffs p) :
     HasNonnegCoeffs p.derivative := by
@@ -34,10 +44,28 @@ example {p : ℝ[X]} (hpos : HasPosLeadingCoeff p) (hdeg : p.natDegree ≠ 0) :
     pos_lc := hpos,
     degree_ne_zero := hdeg
 
+example {p : ℝ[X]} (hpos : HasPosLeadingCoeff p) (hdeg : p.natDegree ≠ 0) :
+    HasPosLeadingCoeff p.derivative := by
+  rr_pos_lc_derivative using
+    pos_lc := hpos
+
+example {p : ℝ[X]} (hpos : HasPosLeadingCoeff p) (hdeg : 2 ≤ p.natDegree) :
+    HasPosLeadingCoeff p.derivative := by
+  rr_pos_lc_derivative using
+    pos_lc := hpos
+
 example {p : ℝ[X]} (hdeg : p.natDegree ≠ 0) :
     p.derivative ≠ 0 := by
   rr_derivative_ne_zero using
     degree_ne_zero := hdeg
+
+example {p : ℝ[X]} (hdeg : p.natDegree ≠ 0) :
+    p.derivative ≠ 0 := by
+  rr_derivative_ne_zero
+
+example {p : ℝ[X]} (hdeg : 2 ≤ p.natDegree) :
+    p.derivative ≠ 0 := by
+  rr_derivative_ne_zero
 
 end Tactic
 end RealRooted
