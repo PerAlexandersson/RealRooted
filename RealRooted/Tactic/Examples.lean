06a96e85ff8b2ef96a2deb6a3408a62e1af5b3e5
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-07-19 16:04:44 +0000

    Add J1 Chebyshev frontend examples

--- RealRooted/Tactic/Examples.lean
+++ RealRooted/Tactic/Examples.lean
@@ -36,6 +36,7 @@ import RealRooted.Tactic.Examples.HermiteBiehler
 import RealRooted.Tactic.Examples.HermitePoulain
 import RealRooted.Tactic.Examples.I2DerivativeLag
 import RealRooted.Tactic.Examples.IteratedDerivativeShift
+import RealRooted.Tactic.Examples.J1Chebyshev
 import RealRooted.Tactic.Examples.J1Gap3Reciprocal
 import RealRooted.Tactic.Examples.Kurtz
 import RealRooted.Tactic.Examples.OEIS
