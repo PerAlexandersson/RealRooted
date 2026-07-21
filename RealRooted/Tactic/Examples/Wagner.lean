4876126ed96b918ad5ce519d868ae4538cf7a5f1
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-07-21 13:22:20 +0000

    Fix Wagner tactic wrappers after signature cleanup

--- RealRooted/Tactic/Examples/Wagner.lean
+++ RealRooted/Tactic/Examples/Wagner.lean
@@ -8,26 +8,26 @@ namespace Tactic
 example {f g h : ℝ[X]}
     (hf : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading f)
     (hg : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading g)
-    (hh : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading h)
+    (_hh : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading h)
     (hfh : Prec f h) (hgh : Prec g h) :
     Prec (f + g) h := by
   rr_wagner_common_right_add using
     left := hf,
     right := hg,
-    common := hh,
+    common := _hh,
     left_interlaces_common := hfh,
     right_interlaces_common := hgh
 
 example {f g h : ℝ[X]}
     (hf : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading f)
     (hg : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading g)
-    (hh : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading h)
+    (_hh : RealRooted.Challenges.Wagner.HasNonposRootsPosLeading h)
     (hhf : Prec h f) (hhg : Prec h g) :
     Prec h (f + g) := by
   rr_wagner_common_left_add using
     left := hf,
     right := hg,
-    common := hh,
+    common := _hh,
     common_interlaces_left := hhf,
     common_interlaces_right := hhg
 
--- RealRooted/Tactic/Wagner.lean
+++ RealRooted/Tactic/Wagner.lean
@@ -17,8 +17,9 @@ theorem wagner_commonRight_add_sequence {F G H : Nat → ℝ[X]}
     (hH : ∀ n : Nat, Challenges.Wagner.HasNonposRootsPosLeading (H n))
     (hFH : ∀ n : Nat, Prec (F n) (H n))
     (hGH : ∀ n : Nat, Prec (G n) (H n)) :
-    ∀ n : Nat, Prec (F n + G n) (H n) := fun n =>
-  Challenges.Wagner.commonRight_add (hF n) (hG n) (hH n) (hFH n) (hGH n)
+    ∀ n : Nat, Prec (F n + G n) (H n) := fun n => by
+  have _ := hH n
+  exact Challenges.Wagner.commonRight_add (hF n) (hG n) (hFH n) (hGH n)
 
 theorem wagner_commonLeft_add_sequence {F G H : Nat → ℝ[X]}
     (hF : ∀ n : Nat, Challenges.Wagner.HasNonposRootsPosLeading (F n))
@@ -26,8 +27,9 @@ theorem wagner_commonLeft_add_sequence {F G H : Nat → ℝ[X]}
     (hH : ∀ n : Nat, Challenges.Wagner.HasNonposRootsPosLeading (H n))
     (hHF : ∀ n : Nat, Prec (H n) (F n))
     (hHG : ∀ n : Nat, Prec (H n) (G n)) :
-    ∀ n : Nat, Prec (H n) (F n + G n) := fun n =>
-  Challenges.Wagner.commonLeft_add (hF n) (hG n) (hH n) (hHF n) (hHG n)
+    ∀ n : Nat, Prec (H n) (F n + G n) := fun n => by
+  have _ := hH n
+  exact Challenges.Wagner.commonLeft_add (hF n) (hG n) (hHF n) (hHG n)
 
 theorem wagner_mulX_iff_sequence {F G : Nat → ℝ[X]}
     (hF : ∀ n : Nat, Challenges.Wagner.HasNonposRootsPosLeading (F n))
@@ -91,22 +93,22 @@ macro_rules
       rr_wagner_common_right_add using
         left := $hf:term,
         right := $hg:term,
-        common := $hh:term,
+        common := $_hh:term,
         left_interlaces_common := $hfh:term,
         right_interlaces_common := $hgh:term) =>
       `(tactic|
         exact RealRooted.Challenges.Wagner.commonRight_add
-          $hf $hg $hh $hfh $hgh)
+          $hf $hg $hfh $hgh)
   | `(tactic|
       rr_wagner_common_left_add using
         left := $hf:term,
         right := $hg:term,
-        common := $hh:term,
+        common := $_hh:term,
         common_interlaces_left := $hhf:term,
         common_interlaces_right := $hhg:term) =>
       `(tactic|
         exact RealRooted.Challenges.Wagner.commonLeft_add
-          $hf $hg $hh $hhf $hhg)
+          $hf $hg $hhf $hhg)
   | `(tactic|
       rr_wagner_mulX_iff using
         shorter := $hf:term,
