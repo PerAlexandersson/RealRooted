8035f7d1501f3d327e36c7adf7c382258a48dbeb
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-07-23 16:28:35 +0000

    Refactor RootBounds sign term macros

--- RealRooted/Tactic/RootBounds.lean
+++ RealRooted/Tactic/RootBounds.lean
@@ -171,6 +171,10 @@ syntax (name := rr_sign_at_roots_sequence_named)
 
 syntax (name := rr_sign_at_roots_term) "rr_sign_at_roots_term " term ", " term : term
 
+syntax (name := rr_sign_at_roots_nonpos_term)
+  "rr_sign_at_roots_nonpos_term " term :
+  term
+
 syntax (name := rr_sign_at_roots_factor)
   "rr_sign_at_roots_with_factor" " using " term ", " term ", " term : tactic
 
@@ -197,6 +201,10 @@ syntax (name := rr_sign_at_roots_factor_term)
   "rr_sign_at_roots_factor_term " term ", " term ", " term :
   term
 
+syntax (name := rr_sign_at_roots_nonpos_factor_term)
+  "rr_sign_at_roots_nonpos_factor_term " term ", " term :
+  term
+
 syntax (name := rr_sign_at_roots_lower)
   "rr_sign_at_roots_lower" " using " term : tactic
 
@@ -221,6 +229,14 @@ syntax (name := rr_sign_at_roots_upper_seq)
   "rr_sign_at_roots_upper_seq " term :
   term
 
+syntax (name := rr_sign_at_roots_lower_term)
+  "rr_sign_at_roots_lower_term " term :
+  term
+
+syntax (name := rr_sign_at_roots_upper_term)
+  "rr_sign_at_roots_upper_term " term :
+  term
+
 syntax (name := rr_sign_at_roots_window)
   "rr_sign_at_roots_window" " using " term ", " term : tactic
 
@@ -229,6 +245,10 @@ syntax (name := rr_sign_at_roots_window_named)
     "root_lower" ":=" term ","
     "root_upper" ":=" term :
   tactic
+
+syntax (name := rr_sign_at_roots_window_term)
+  "rr_sign_at_roots_window_term " term ", " term :
+  term
 
 macro_rules
   | `(tactic| rr_root_nonpos using $hrr:term, $hnn:term, $hr:term) =>
@@ -298,10 +318,7 @@ macro_rules
           $hrr $hnn $hder_ne)
   | `(tactic| rr_sign_at_roots using $hrr:term, $hnn:term) =>
       `(tactic|
-        exact fun r hroot => by
-          have hroot_nonpos : r ≤ 0 := by
-            rr_root_nonpos using $hrr, $hnn, hroot
-          rr_sign)
+        exact (rr_sign_at_roots_term $hrr, $hnn))
   | `(tactic|
       rr_sign_at_roots using
         realrooted := $hrr:term,
@@ -309,7 +326,12 @@ macro_rules
       `(tactic|
         rr_sign_at_roots using $hrr, $hnn)
   | `(rr_sign_at_roots_term $hrr:term, $hnn:term) =>
-      `(by rr_sign_at_roots using $hrr, $hnn)
+      `(rr_sign_at_roots_nonpos_term
+          (RealRooted.roots_nonpos_of_realrooted_of_nonneg_coeffs $hrr $hnn))
+  | `(rr_sign_at_roots_nonpos_term $hroot_nonpos:term) =>
+      `(fun r hroot => by
+          have hroot_nonpos : r ≤ 0 := $hroot_nonpos r hroot
+          rr_sign)
   | `(tactic| rr_sign_at_roots) =>
       `(tactic|
         exact fun r hroot => by
@@ -321,19 +343,14 @@ macro_rules
         realrooted := $hrr:term,
         nonneg := $hnn:term) =>
       `(tactic|
-        exact fun n r hroot => by
-          have hroot_nonpos : r ≤ 0 :=
-            (RealRooted.roots_nonpos_sequence_of_realrooted_of_nonneg_coeffs
-              $hrr $hnn) n r hroot
-          rr_sign)
+        exact fun n =>
+          (rr_sign_at_roots_nonpos_term
+            ((RealRooted.roots_nonpos_sequence_of_realrooted_of_nonneg_coeffs
+              $hrr $hnn) n)))
   | `(tactic|
       rr_sign_at_roots_with_factor using $hrr:term, $hnn:term, $hq:term) =>
       `(tactic|
-        exact fun r hroot => by
-          have hroot_nonpos : r ≤ 0 := by
-            rr_root_nonpos using $hrr, $hnn, hroot
-          have hfactor_nonneg := $hq r hroot
-          rr_sign)
+        exact (rr_sign_at_roots_factor_term $hrr, $hnn, $hq))
   | `(tactic|
       rr_sign_at_roots_with_factor using
         realrooted := $hrr:term,
@@ -342,7 +359,14 @@ macro_rules
       `(tactic|
         rr_sign_at_roots_with_factor using $hrr, $hnn, $hq)
   | `(rr_sign_at_roots_factor_term $hrr:term, $hnn:term, $hq:term) =>
-      `(by rr_sign_at_roots_with_factor using $hrr, $hnn, $hq)
+      `(rr_sign_at_roots_nonpos_factor_term
+          (RealRooted.roots_nonpos_of_realrooted_of_nonneg_coeffs $hrr $hnn),
+          $hq)
+  | `(rr_sign_at_roots_nonpos_factor_term $hroot_nonpos:term, $hq:term) =>
+      `(fun r hroot => by
+          have hroot_nonpos : r ≤ 0 := $hroot_nonpos r hroot
+          have hfactor_nonneg := $hq r hroot
+          rr_sign)
   | `(tactic|
       rr_sign_at_roots_with_factor using
         factor_nonneg := $hq:term) =>
@@ -358,18 +382,15 @@ macro_rules
         nonneg := $hnn:term,
         factor_nonneg := $hq:term) =>
       `(tactic|
-        exact fun n r hroot => by
-          have hroot_nonpos : r ≤ 0 :=
-            (RealRooted.roots_nonpos_sequence_of_realrooted_of_nonneg_coeffs
-              $hrr $hnn) n r hroot
-          have hfactor_nonneg := $hq n r hroot
-          rr_sign)
+        exact fun n =>
+          (rr_sign_at_roots_nonpos_factor_term
+            ((RealRooted.roots_nonpos_sequence_of_realrooted_of_nonneg_coeffs
+              $hrr $hnn) n),
+            ($hq n)))
   | `(tactic|
       rr_sign_at_roots_lower using $hlo:term) =>
       `(tactic|
-        exact fun r hroot => by
-          have hroot_lower := $hlo r hroot
-          rr_sign)
+        exact (rr_sign_at_roots_lower_term $hlo))
   | `(tactic|
       rr_sign_at_roots_lower using
         root_lower := $hlo:term) =>
@@ -378,31 +399,39 @@ macro_rules
   | `(tactic|
       rr_sign_at_roots_upper using $hhi:term) =>
       `(tactic|
-        exact fun r hroot => by
-          have hroot_upper := $hhi r hroot
-          rr_sign)
+        exact (rr_sign_at_roots_upper_term $hhi))
   | `(tactic|
       rr_sign_at_roots_upper using
         root_upper := $hhi:term) =>
       `(tactic|
         rr_sign_at_roots_upper using $hhi)
   | `(rr_sign_at_roots_lower_seq $hlo:term) =>
-      `(fun n => by rr_sign_at_roots_lower using ($hlo n))
+      `(fun n => (rr_sign_at_roots_lower_term ($hlo n)))
   | `(rr_sign_at_roots_upper_seq $hhi:term) =>
-      `(fun n => by rr_sign_at_roots_upper using ($hhi n))
+      `(fun n => (rr_sign_at_roots_upper_term ($hhi n)))
+  | `(rr_sign_at_roots_lower_term $hlo:term) =>
+      `(fun r hroot => by
+          have hroot_lower := $hlo r hroot
+          rr_sign)
+  | `(rr_sign_at_roots_upper_term $hhi:term) =>
+      `(fun r hroot => by
+          have hroot_upper := $hhi r hroot
+          rr_sign)
   | `(tactic|
       rr_sign_at_roots_window using $hlo:term, $hhi:term) =>
       `(tactic|
-        exact fun r hroot => by
-          have hroot_lower := $hlo r hroot
-          have hroot_upper := $hhi r hroot
-          rr_sign)
+        exact (rr_sign_at_roots_window_term $hlo, $hhi))
   | `(tactic|
       rr_sign_at_roots_window using
         root_lower := $hlo:term,
         root_upper := $hhi:term) =>
       `(tactic|
         rr_sign_at_roots_window using $hlo, $hhi)
+  | `(rr_sign_at_roots_window_term $hlo:term, $hhi:term) =>
+      `(fun r hroot => by
+          have hroot_lower := $hlo r hroot
+          have hroot_upper := $hhi r hroot
+          rr_sign)
 
 end Tactic
 end RealRooted
