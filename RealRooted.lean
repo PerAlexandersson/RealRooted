04e9abb84f804b632cccf4dd9c6e46a9868f7316
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-08-02 03:43:11 +0000

    chore: expose finite-symbol linearity

--- RealRooted.lean
+++ RealRooted.lean
@@ -23,6 +23,7 @@ import RealRooted.Basic
 import RealRooted.Bezoutian
 import RealRooted.BorceaBranden.BoundarySpecialization
 import RealRooted.BorceaBranden.FiniteSymbolCoefficient
+import RealRooted.BorceaBranden.FiniteSymbolLinearity
 import RealRooted.BorceaBranden.Applications.BidiagonalSymbol
 import RealRooted.BorceaBranden.Applications.HomogenizeStable
 import RealRooted.BorceaBranden.Applications.UnivariateSymbol
