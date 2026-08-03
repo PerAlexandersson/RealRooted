48c3d233ca15276f1b891635d5bb73ad78a8c8e6
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-08-03 05:11:08 +0000

    Golf filtered erasure proof

--- RealRooted/Mathlib/Data/List/Basic.lean
+++ RealRooted/Mathlib/Data/List/Basic.lean
@@ -18,13 +18,9 @@ theorem filter_eraseIdx_eq_of_getElem_not
     {α : Type*} {p : α → Bool} {l : List α} {i : ℕ}
     (hi : i < l.length) (hpi : ¬p l[i]) :
     (l.eraseIdx i).filter p = l.filter p := by
-  calc
-    (l.eraseIdx i).filter p =
-        (l.take i ++ l.drop (i + 1)).filter p := by
-      rw [List.eraseIdx_eq_take_drop_succ]
-    _ = (l.take i ++ l[i] :: l.drop (i + 1)).filter p := by
-      simp only [List.filter_append, List.filter_cons_of_neg hpi]
-    _ = l.filter p := by
-      rw [← List.drop_eq_getElem_cons hi, List.take_append_drop]
+  rw [List.eraseIdx_eq_take_drop_succ, List.filter_append]
+  conv_rhs =>
+    rw [← List.take_append_drop i l, List.filter_append,
+      List.drop_eq_getElem_cons hi, List.filter_cons_of_neg hpi]
 
 end List
