0226016c790035141857e80a3aa27b0d03eb2133
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-08-03 08:29:05 +0000

    Deduplicate unique equivalence degree proof

--- RealRooted/Mathlib/Algebra/MvPolynomial/Equiv.lean
+++ RealRooted/Mathlib/Algebra/MvPolynomial/Equiv.lean
@@ -10,20 +10,6 @@ This file contains Mathlib-shaped compatibility lemmas for
 
 namespace MvPolynomial
 
-/-- Passing from a uniquely indexed multivariate polynomial to a univariate
-polynomial does not increase total degree. -/
-theorem natDegree_uniqueAlgEquiv_le_totalDegree
-    {σ R : Type*} [Unique σ] [CommSemiring R] (p : MvPolynomial σ R) :
-    (MvPolynomial.uniqueAlgEquiv R σ p).natDegree ≤ p.totalDegree := by
-  rw [Polynomial.natDegree_le_iff_coeff_eq_zero]
-  intro n hn
-  rw [MvPolynomial.coeff_uniqueAlgEquiv]
-  by_contra hcoeff
-  have hsupp : Finsupp.single default n ∈ p.support :=
-    MvPolynomial.mem_support_iff.mpr hcoeff
-  have hle := MvPolynomial.le_totalDegree hsupp
-  exact (not_le_of_gt hn) (by simpa using hle)
-
 /-- The degree in the unique variable after converting a univariate polynomial
 to an `MvPolynomial` is its natural degree. -/
 theorem degreeOf_uniqueAlgEquiv_symm
@@ -48,5 +34,20 @@ theorem degreeOf_uniqueAlgEquiv_symm
     rw [MvPolynomial.coeff_uniqueAlgEquiv_symm R p
       (Finsupp.single default n)] at hzero
     simpa using hzero
+
+/-- Passing from a uniquely indexed multivariate polynomial to a univariate
+polynomial does not increase total degree. -/
+theorem natDegree_uniqueAlgEquiv_le_totalDegree
+    {σ R : Type*} [Unique σ] [CommSemiring R] (p : MvPolynomial σ R) :
+    (MvPolynomial.uniqueAlgEquiv R σ p).natDegree ≤ p.totalDegree := by
+  calc
+    (MvPolynomial.uniqueAlgEquiv R σ p).natDegree =
+        degreeOf default
+          ((MvPolynomial.uniqueAlgEquiv R σ).symm
+            (MvPolynomial.uniqueAlgEquiv R σ p)) :=
+      (degreeOf_uniqueAlgEquiv_symm
+        (MvPolynomial.uniqueAlgEquiv R σ p)).symm
+    _ = degreeOf default p := by rw [AlgEquiv.symm_apply_apply]
+    _ ≤ p.totalDegree := degreeOf_le_totalDegree p default
 
 end MvPolynomial
