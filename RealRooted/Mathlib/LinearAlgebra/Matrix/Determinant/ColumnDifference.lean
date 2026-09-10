2c97a4ae4e2e579e0b759b0be3fd172baac0f3f8
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-09-10 15:08:03 +0000

    feat: expand constant-row column differences

--- RealRooted/Mathlib/LinearAlgebra/Matrix/Determinant/ColumnDifference.lean
+++ RealRooted/Mathlib/LinearAlgebra/Matrix/Determinant/ColumnDifference.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Fin.Rev
 import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
+import RealRooted.Mathlib.LinearAlgebra.Matrix.Determinant.Basic
 
 /-!
 # Determinant-preserving adjacent column differences
@@ -36,5 +37,32 @@ theorem det_columnDifference {R : Type*} [CommRing R] {N : ℕ}
       rw [Fin.rev_succ, Fin.rev_castSucc]
       simp [columnDifference])
   simpa using hdet.symm
+
+/-- Expanding a matrix with constant final row after simultaneous adjacent
+column differences. -/
+theorem det_eq_last_apply_mul_det_adjacentColumnDifference_of_lastRow_eq
+    {R : Type*} [CommRing R] {n : ℕ}
+    (A : Matrix (Fin (n + 1)) (Fin (n + 1)) R) (c : R)
+    (hrow : ∀ j, A (Fin.last n) j = c) :
+    A.det = c * (Matrix.of fun (i j : Fin n) =>
+      A i.castSucc j.castSucc - A i.castSucc j.succ).det := by
+  calc
+    A.det = (columnDifference A).det := (det_columnDifference A).symm
+    _ = (columnDifference A).transpose.det := by rw [det_transpose]
+    _ = (columnDifference A).transpose (Fin.last n) (Fin.last n) *
+        ((columnDifference A).transpose.submatrix Fin.castSucc Fin.castSucc).det :=
+      det_eq_last_apply_mul_det_castSucc_of_above_eq_zero _ (by
+        intro i
+        simp [columnDifference, hrow])
+    _ = c * (Matrix.of fun (i j : Fin n) =>
+        A i.castSucc j.castSucc - A i.castSucc j.succ).det := by
+      have hblock : (columnDifference A).submatrix Fin.castSucc Fin.castSucc =
+          Matrix.of fun (i j : Fin n) =>
+            A i.castSucc j.castSucc - A i.castSucc j.succ := by
+        ext i j
+        simp [columnDifference]
+      rw [← det_transpose]
+      rw [transpose_submatrix, transpose_transpose, hblock]
+      simp [columnDifference, hrow]
 
 end Matrix
