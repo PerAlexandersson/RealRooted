9d79bc41bcb2bb374a6962e9d4bbe9426079a4d4
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-09-12 20:03:40 +0000

    feat: prove signed-word run statistics (#657)

--- RealRooted/Production.lean
+++ RealRooted/Production.lean
@@ -113,6 +113,7 @@ import RealRooted.BrandenVecchi.ChowTotallyNonneg
 import RealRooted.BrandenVecchi.ReflectionInterlacing
 import RealRooted.BrandenVecchi.SignedWordEnumerator
 import RealRooted.BrandenVecchi.SignedWordRuns.Basic
+import RealRooted.BrandenVecchi.SignedWordRuns.Statistics
 import RealRooted.BrandenVecchi.SignedWords
 import RealRooted.BrandenVecchi.SupersymmetricCoefficients
 import RealRooted.BrandenLeite.Resolvable
